7a8ae3faaa205d658f23219d63790a1f50f6c1dd
Damien Mathieu <42@dmathieu.com>
2021-07-27 16:05:30 +0200

    add database cleaner

--- app/indexer/municipales/annee_2020.rb
+++ app/indexer/municipales/annee_2020.rb
@@ -2,6 +2,15 @@ module VR
   module Indexer
     module Municipales
       class Annee2020 < Base
+        set_keymap({
+          inscrits: { index: 5 },
+          abstentions: { index: 6 },
+          votants: { index: 8 },
+          blancs: { index: 10 },
+          nuls: { index: 13 },
+          exprimes: { index: 16 },
+        })
+
       end
     end
   end
---
+++ spec/indexer/municipales/annee_2020_spec.rb
@@ -0,0 +1,35 @@
+describe VR::Indexer::Municipales::Annee2020 do
+  subject { described_class.new({name: "Municipales 2020"}, mapper) }
+  let(:mapper) { VR::Mapper::Roo.new(config) }
+  let(:config) {
+    {
+      data: [
+        {
+          name: "1er tour",
+          url: "https://example.com/tour-1"
+        },
+        {
+          name: "2e tour",
+          url: "https://example.com/tour-2"
+        }
+      ],
+      format: "xlsx"
+    }
+  }
+  let(:data) { file_fixture("municipales.xlsx") }
+
+  before :each do
+    stub_request(:get, "https://example.com/tour-1")
+      .to_return(status: 200, body: file_fixture("municipales_2020/tour-1.xlsx"), headers: {})
+    stub_request(:get, "https://example.com/tour-2")
+      .to_return(status: 200, body: file_fixture("municipales_2020/tour-2.xlsx"), headers: {})
+  end
+
+  describe "run" do
+    it "inserts the event" do
+      expect do
+        subject.run
+      end.to change(Event, :count).by(1)
+    end
+  end
+end
