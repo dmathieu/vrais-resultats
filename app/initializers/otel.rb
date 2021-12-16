ec92082d99efc6fe542b3aeb294fbbfc80acc805
Damien Mathieu <42@dmathieu.com>
2021-12-16 15:33:32 +0100

    use otlp exporter

--- app/initializers/otel.rb
+++ app/initializers/otel.rb
@@ -1,21 +1,25 @@
 require "opentelemetry"
 require "opentelemetry-api"
 require "opentelemetry-sdk"
-require "opentelemetry/exporters/honeycomb"
+require "opentelemetry/exporter/otlp"
 
 require "faraday"
 require "faraday_middleware"
 require "opentelemetry-instrumentation-faraday"
 
 OpenTelemetry::SDK.configure do |c|
+  c.service_name = "vrais-resultats"
   c.use_all
 
   if ENV.key?("HONEYCOMB_API_KEY")
     c.add_span_processor(
       OpenTelemetry::SDK::Trace::Export::BatchSpanProcessor.new(
-        OpenTelemetry::Exporters::Honeycomb::Exporter.new(
-          writekey: ENV["HONEYCOMB_API_KEY"],
-          dataset: "vrais-resultats"
+        OpenTelemetry::Exporter::OTLP::Exporter.new(
+          endpoint: "https://api.honeycomb.io/v1/traces",
+          headers: {
+            "x-honeycomb-team": ENV["HONEYCOMB_API_KEY"],
+            "x-honeycomb-dataset": "vrais-resultats"
+          }
         )
       )
     )
