c98e62515a4eb0a7b189400e56429d3f5a1c8c9e
Damien Mathieu <damien.mathieu@okta.com>
2022-02-23 15:32:34 +0100

    add presidentielles 2007 and 2002

--- app/reducer/presidentielles.rb
+++ app/reducer/presidentielles.rb
@@ -7,3 +7,5 @@ end
 
 require "reducer/presidentielles/annee_2017"
 require "reducer/presidentielles/annee_2012"
+require "reducer/presidentielles/annee_2007"
+require "reducer/presidentielles/annee_2002"
---
+++ app/reducer/presidentielles/annee_2002.rb
@@ -0,0 +1,41 @@
+module VR
+  module Reducer
+    class Presidentielles::Annee2002 < Presidentielles
+      private
+
+      def keymap
+        [
+          {key: :inscrits, index: 3, default: 0},
+          {key: :abstentions, fn: lambda { |r| r[3].to_i - r[4].to_i }, default: 0},
+          {key: :votants, index: 4, default: 0},
+          {key: :nuls, index: 6, default: 0},
+          {key: :exprimes, index: 5, default: 0}
+        ]
+      end
+
+      def skip_row_if(i, row)
+        i <= 3
+      end
+
+      def row_name(row)
+        ""
+      end
+
+      def row_path(row)
+        ""
+      end
+
+      def header_row() = 0
+
+      def candidats_split(entry)
+        @header.drop(7).each_with_index do |c, i|
+          yield({
+            nom: c,
+            voix: entry[i + 7].to_i,
+            liste: ""
+          })
+        end
+      end
+    end
+  end
+end
