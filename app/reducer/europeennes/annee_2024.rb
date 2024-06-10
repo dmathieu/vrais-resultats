e071b4ab2df816866f2c9f2a3c829d0462838686
dmathieu <42@dmathieu.com>
2024-06-10 14:31:50 +0200

    start europeennes 2024 template

---
+++ app/reducer/europeennes/annee_2024.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+module VR
+  module Reducer
+    class Europeennes
+      class Annee2024 < Europeennes
+        private
+
+        def keymap
+          [
+            { key: :inscrits, index: 2, default: 0 },
+            { key: :abstentions, index: 3, default: 0 },
+            { key: :votants, index: 5, default: 0 },
+            { key: :blancs, index: 7, default: 0 },
+            { key: :nuls, index: 10, default: 0 },
+            { key: :exprimes, index: 13, default: 0 }
+          ]
+        end
+
+        def skip_row_if(index, _row)
+          index < 1
+        end
+
+        def row_name(_row)
+          ''
+        end
+
+        def row_path(_row)
+          ''
+        end
+
+        def default_hash(_entry, name)
+          h = {
+            candidats: [],
+            name:
+          }
+
+          KEYMAP.each do |k|
+            h[k[:key]] = k[:default]
+          end
+
+          h
+        end
+
+        def candidats_split(entry)
+          entry.drop(16).each_slice(7).each do |c|
+            yield({
+              nom: c[3],
+              voix: c[4],
+              liste: c[2]
+            })
+          end
+        end
+      end
+    end
+  end
+end
